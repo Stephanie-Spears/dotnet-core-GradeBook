04d8c736d8c44fae88f7fbe2ed5df0bd39db5a75
Stephanie Spears <stephanie.spears1@gmail.com>
2021-01-11 08:20:14 -0800

    refactor classes

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -50,14 +50,30 @@ namespace GradeBook
 
 		public override void AddGrade(double grade)
 		{
-			var writer = File.AppendText($"{Name}.txt");
-			writer.WriteLine(grade);
-			writer.Close();
+			using (var writer = File.AppendText($"{Name}.txt"))
+			{
+				writer.WriteLine(grade);
+				if (GradeAdded != null)
+				{
+					GradeAdded(this, new EventArgs());
+				}
+			}
 		}
 
 		public override Statistics GetStatistics()
 		{
-			throw new NotImplementedException();
+			var result = new Statistics();
+			using (var reader = File.OpenText($"{Name}.txt"))
+			{
+				var line = reader.ReadLine();
+				while (line != null)
+				{
+					var number = double.Parse(line);
+					result.Add(number);
+					line = reader.ReadLine();
+				}
+			}
+			return result;
 		}
 	}
 
@@ -70,28 +86,6 @@ namespace GradeBook
 		    grades = new List<double>();
 		    Name = name;
 	    }
-
-
-	    public void AddGrade(char letter)
-	    {
-		    switch (letter)
-		    {
-				case 'A':
-					AddGrade(90);
-					break;
-				case 'B':
-					AddGrade(80);
-					break;
-				case 'C':
-					AddGrade(70);
-					break;
-				default: 
-					AddGrade(0);
-					break;
-		    }
-
-	    }
-
 
 	    public override void AddGrade(double grade)
 	    {
@@ -115,36 +109,10 @@ namespace GradeBook
 	    public override Statistics GetStatistics()
 	    {
 		    var result = new Statistics();
-		    result.Average = 0.0;
-		    result.High = double.MinValue;
-		    result.Low = double.MaxValue;
 
-		    foreach (var grade in grades)
+		    for (var index = 0; index < grades.Count; index += 1 )
 		    {
-			    result.Low = Math.Min(grade, result.Low);
-			    result.High = Math.Max(grade, result.High);
-			    result.Average += grade;
-		    }
-
-		    result.Average /= grades.Count;
-
-		    switch (result.Average)
-		    {
-			    case var d when d >= 90.0:
-				    result.Letter = 'A';
-				    break;
-			    case var d when d >= 80.0:
-				    result.Letter = 'B';
-				    break;
-			    case var d when d >= 70.0:
-				    result.Letter = 'C';
-				    break;
-			    case var d when d >= 60.0:
-				    result.Letter = 'D';
-				    break;
-			    default:
-				    result.Letter = 'F';
-				    break;
+				result.Add(grades[index]);
 		    }
 
 		    return result;
@@ -152,6 +120,5 @@ namespace GradeBook
 
 	    private List<double> grades;
 
-	    public const string CATEGORY = "Science";
     }
 }
